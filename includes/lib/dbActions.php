8cbfc426ee10191584ca4e7c6bd22120848e7f88
40chalk <cstelum8aegis@outlook.com>
2022-04-16 12:26:46 -0500

    refactor db actions

--- includes/lib/dbActions.php
+++ includes/lib/dbActions.php
@@ -2,32 +2,36 @@
 
 namespace MyOwnPub\Includes\Lib\DbActions;
 
-function createUniverseTable() {
+function queryCreateTable( $tableName, $query ): void {
 	global $wpdb;
-	$charset = $wpdb->get_charset_collate();
-	$tableName = $wpdb->prefix . "myop_universe";
-	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-
-	dbDelta("CREATE TABLE $tableName (
-      id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-      name varchar(60) NOT NULL DEFAULT '',
-      options longtext NOT NULL DEFAULT '',
-      works longtext NOT NULL DEFAULT '',
-      PRIMARY KEY  (id)
-    ) $charset;");
+	$charset   = $wpdb->get_charset_collate();
+	$tableName = $wpdb->prefix . $tableName;
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( queries: "CREATE TABLE $tableName ($query) $charset;" );
 }
 
-function createChaptersTable() {
-	global $wpdb;
-	$charset = $wpdb->get_charset_collate();
-	$tableName = $wpdb->prefix . "myop_chapters";
-	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+$universeTableQuery = "
+	id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	name varchar(60) NOT NULL DEFAULT '',
+	options longtext NOT NULL DEFAULT '',
+	works longtext NOT NULL DEFAULT '',
+	PRIMARY KEY  (id)
+";
+queryCreateTable( tableName: 'myop_universe', query: $universeTableQuery );
 
-	dbDelta("CREATE TABLE $tableName (
-      id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-      name varchar(60) NOT NULL DEFAULT '',
-      options longtext NOT NULL DEFAULT '',
-      text longtext NOT NULL DEFAULT '',
-      PRIMARY KEY  (id)
-    ) $charset;");
+
+$chaptersTableQuery = "
+	id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+    name varchar(60) NOT NULL DEFAULT '',
+    options longtext NOT NULL DEFAULT '',
+    text longtext NOT NULL DEFAULT '',
+    PRIMARY KEY  (id)
+";
+queryCreateTable( tableName: 'myop_chapters', query: $chaptersTableQuery );
+
+
+function dbBackupToJson() {
+}
+
+function dbRestoreFromJson() {
 }
